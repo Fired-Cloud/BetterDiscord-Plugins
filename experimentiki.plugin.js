9c5f378e17c5cabeb5ef99b11da9bcb0bf59a7d4
Aye <70176152+ayebrian@users.noreply.github.com>
2022-09-11 19:52:12 +0300

    sussy

---
+++ experimentiki.plugin.js
@@ -0,0 +1,31 @@
+/**
+ * @name discordExperiments_v2
+ * @description Enables the experiments tab in discord's settings.
+ * @author Aye
+ * @version 2.0
+ * @website https://betterdiscord.app/plugin/Discord%20Experiments
+ * @source https://github.com/Inve1951/BetterDiscordStuff/blob/master/plugins/discordexperiments.plugin.js
+ * @updateUrl https://betterdiscord.app/gh-redirect?id=206
+ */
+
+ const settingsStore = BdApi.findModule(m => typeof m?.default?.isDeveloper !== "undefined");
+ const userStore = BdApi.findModule(m => m?.default?.getUsers);
+ 
+ module.exports = class {
+     getName(){ return "Discord Experiments"; }
+ 
+     start() {
+         const nodes = Object.values(settingsStore.default._dispatcher._actionHandlers._dependencyGraph.nodes);
+         try {
+             nodes.find(x => x.name == "ExperimentStore").actionHandler["CONNECTION_OPEN"]({user: {flags: 1}, type: "CONNECTION_OPEN"})
+         } catch (e) {} // this will always intentionally throw
+         const oldGetUser = userStore.default.__proto__.getCurrentUser;
+         userStore.default.__proto__.getCurrentUser = () => ({hasFlag: () => true})
+         nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+         userStore.default.__proto__.getCurrentUser = oldGetUser
+     }
+ 
+     stop(){
+         Object.values(settingsStore.default._dispatcher._dependencyGraph.nodes).find(x => x.name == "ExperimentStore").actionHandler["CONNECTION_OPEN"]({user: {flags: 0}, type: "CONNECTION_OPEN"})
+     }
+ };
